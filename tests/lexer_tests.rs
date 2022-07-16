bf69c48eae0e8618bbfbaad9ecad9fd67ad2b0e2
Thomas <thomas.donst@gmx.de>
2022-07-16 21:28:40 +0200

    Extended lexer and parser tests

--- tests/lexer_tests.rs
+++ tests/lexer_tests.rs
@@ -1,23 +1,21 @@
-#![allow(warnings)]
-
 use shell::config::FOLDER_NAME;
 use shell::lexer::Lexer;
 use shell::token::Token;
+use shell::utils::get_program_dir;
 
-fn get_tokens(input: &str) -> Vec<Token>{
-    let mut lexer = Lexer::new(input, FOLDER_NAME);
+// NOTE: cat, grep, seq and echo should exist in the programs directory in order to pass the tests
+
+fn get_tokens(input: &str) -> Vec<Token> {
+    let program_dir = get_program_dir();
+    let mut lexer = Lexer::new(input, &program_dir);
     lexer.get_tokens()
 }
 
 #[test]
 fn lex_symbols_test() {
-    let input = "> >>   >& <  << & && | = \" ;";
     let expected_tokens = vec![
         Token::Great,
-        Token::DoubleGreat,
-        Token::GreatAmpersand,
         Token::Less,
-        Token::DoubleLess,
         Token::Ampersand,
         Token::DoubleAmpersand,
         Token::Pipe,
@@ -25,34 +23,73 @@ fn lex_symbols_test() {
         Token::Quote,
         Token::Semicolon,
     ];
-    assert_eq!(get_tokens(input), expected_tokens);
-}
-
-#[test]
-fn lex_commands_test() {
-    let input = "Cd Clear";
-    let expected_tokens = vec![
-        Token::Command("cd".to_string()),
-        Token::Command("clear".to_string()),
-    ];
-    assert_eq!(get_tokens(input), expected_tokens);
+    assert_eq!(get_tokens("> < & && | = \" ;"), expected_tokens);
+    assert_eq!(get_tokens("><& &&|=\";"), expected_tokens);
 }
 
 #[test]
 fn lex_arguments_test() {
-    let input = "Only arguments";
     let expected_tokens = vec![
         Token::Argument("only".to_string()),
         Token::Argument("arguments".to_string()),
     ];
-    assert_eq!(get_tokens(input), expected_tokens);
+    assert_eq!(get_tokens("Only arguments"), expected_tokens);
 }
 
 #[test]
-fn lex_hyphens_test() {}
+fn lex_hyphens_test() {
+    let expected_tokens = vec![
+        Token::Hyphen("a".to_string()),
+        Token::Hyphen("abc".to_string()),
+        Token::Hyphen("".to_string()),
+    ];
+    assert_eq!(get_tokens("-a -abc-"), expected_tokens);
+}
 
 #[test]
-fn lex_double_hyphens_test() {}
+fn lex_double_hyphens_test() {
+    let expected_tokens = vec![
+        Token::DoubleHyphen("option".to_string()),
+        Token::DoubleHyphen("".to_string()),
+        Token::DoubleHyphen("option2".to_string()),
+    ];
+    assert_eq!(get_tokens("--option-- --option2"), expected_tokens);
+}
 
 #[test]
-fn lex_env_variables_test() {}
+fn lex_env_variables_test() {
+    let expected_tokens = vec![
+        Token::EnvVariable("var".to_string()),
+        Token::EnvVariable("".to_string()),
+        Token::EnvVariable("".to_string()),
+        Token::EnvVariable("var2".to_string()),
+    ];
+    assert_eq!(get_tokens("$var$$$var2"), expected_tokens);
+}
+
+#[test]
+fn lex_builtin_commands_test() {
+    let expected_tokens = vec![
+        Token::Command("cd".to_string()),
+        Token::Command("clear".to_string()),
+    ];
+    assert_eq!(get_tokens("Cd Clear"), expected_tokens);
+}
+
+#[test]
+fn lex_complex_command_test() {
+    let expected_tokens = vec![
+        Token::Command("cat".to_string()),
+        Token::Argument("a.txt".to_string()),
+        Token::Pipe,
+        Token::Command("grep".to_string()),
+        Token::Argument("h".to_string()),
+        Token::DoubleAmpersand,
+        Token::Command("seq".to_string()),
+        Token::Argument("3".to_string()),
+        Token::Semicolon,
+        Token::Command("echo".to_string()),
+        Token::EnvVariable("hello".to_string()),
+    ];
+    assert_eq!(get_tokens("cat a.txt | grep h &&seq 3 ; echo $hello"), expected_tokens);
+}
--- tests/parser_tests.rs
+++ tests/parser_tests.rs
@@ -1,5 +1,122 @@
 #![allow(warnings)]
 
+use shell::ast::{Expr, Operator};
+use shell::ast::Expr::{Binary, Cmd};
+use shell::utils::{get_program_dir, parse};
+
+// NOTE: cat, echo, grep, fmt and seq should exist in the programs directory in order to pass this test.
+// NOTE: q should NOT exist in the programs directory ...
+
+fn parse_input(input: &str) -> Result<Expr, String> {
+    let program_dir = get_program_dir();
+    parse(input, &program_dir)
+}
 
 #[test]
-fn x() {}
+fn parse_commands_test() {
+    let expected_ast = Cmd {
+        program: "cat".to_string(),
+        arguments: vec!["./tests/files/tmp.txt".to_string()],
+    };
+    assert_eq!(parse_input("cat ./tests/files/tmp.txt").unwrap(), expected_ast);
+
+    let expected_ast = Cmd {
+        program: "echo".to_string(),
+        arguments: vec!["123".to_string(), "456\n".to_string(), "abc".to_string()],
+    };
+    assert_eq!(parse_input("echo 123 456\n abc").unwrap(), expected_ast);
+}
+
+#[test]
+fn parse_argument_test() {
+    let expected_ast = Expr::Argument("q".to_string());
+    assert_eq!(parse_input("q").unwrap(), expected_ast);
+}
+
+#[test]
+fn parse_binary_test() {
+    let expected_ast = Binary(
+        Box::new(Cmd {
+            program: "cat".to_string(),
+            arguments: vec!["./tests/files/tmp.txt".to_string()],
+        }),
+        Operator::Pipe,
+        Box::new(Cmd {
+            program: "grep".to_string(),
+            arguments: vec!["1".to_string()],
+        }),
+    );
+    assert_eq!(parse_input("cat ./tests/files/tmp.txt | grep 1").unwrap(), expected_ast);
+}
+
+#[test]
+fn parse_complex_binaries_test() {
+    let expected_ast = Binary(
+        Box::new(Binary(
+            Box::new(Binary(
+                Box::new(Cmd {
+                    program: "cat".to_string(),
+                    arguments: vec!["./f".to_string()],
+                }),
+                Operator::Pipe,
+                Box::new(Cmd {
+                    program: "fmt".to_string(),
+                    arguments: vec![],
+                }),
+            )),
+            Operator::And,
+            Box::new(Cmd {
+                program: "echo".to_string(),
+                arguments: vec!["ok".to_string()],
+            }),
+        )),
+        Operator::Next,
+        Box::new(Cmd {
+            program: "echo".to_string(),
+            arguments: vec!["next".to_string()],
+        }),
+    );
+    assert_eq!(parse_input("cat ./f | fmt && echo ok ; echo next").unwrap(), expected_ast);
+
+
+    let expected_ast = Binary(
+        Box::new(Binary(
+            Box::new(Binary(
+                Box::new(Cmd {
+                    program: "echo".to_string(),
+                    arguments: vec!["123".to_string()],
+                }),
+                Operator::And,
+                Box::new(Binary(
+                    Box::new(Binary(
+                        Box::new(Cmd {
+                            program: "echo".to_string(),
+                            arguments: vec!["456".to_string()],
+                        }),
+                        Operator::Pipe,
+                        Box::new(Cmd {
+                            program: "grep".to_string(),
+                            arguments: vec!["4".to_string()],
+                        }),
+                    )),
+                    Operator::Pipe,
+                    Box::new(Cmd {
+                        program: "fmt".to_string(),
+                        arguments: vec![],
+                    }),
+                )),
+            )),
+            Operator::And,
+            Box::new(Cmd {
+                program: "seq".to_string(),
+                arguments: vec!["3".to_string()],
+            }),
+        )),
+        Operator::Next,
+        Box::new(Cmd {
+            program: "echo".to_string(),
+            arguments: vec![],
+        }),
+    );
+    assert_eq!(parse_input("echo 123 && echo 456 | grep 4 | fmt && seq 3 ; echo").unwrap(), expected_ast);
+}
